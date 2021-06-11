32d85a4ee5d62f42e2d4bce1c651a4bd27e9ad0f
Chong Sun <chong.sun@fdmgroup.com>
2021-06-11 11:10:08 +1000

    _

---
+++ Desktop/csharpWorkSpace/CF/CF/Fay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CF
+{
+    public class Fay
+    {
+        public Fay()
+        {
+            static void Main(string[] args)
+            {
+                // Display the number of command line arguments.
+                Console.WriteLine(args.Length);
+            }
+
+        }
+    }
+}
---
+++ Desktop/csharpWorkSpace/CF/CF/Love.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CF
+{
+    public static class Love
+    {
+        public void Hello()
+        {
+            Console.WriteLine("asdf");
+        }
+    }
+}
